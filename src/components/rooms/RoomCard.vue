7710f7d168d3d84c8ca30b49a308a7e0067f2a2b
jonasdegener <jjdegener@gmail.com>
2020-12-10 16:15:13 +0100

    get data from firebase -> view -> component

---
+++ src/components/rooms/RoomCard.vue
@@ -0,0 +1,20 @@
+<template>
+<div>
+    <h1>card</h1>
+</div>
+</template>
+
+<script>
+export default {
+  props: {
+    card: {}
+  },
+  mounted () {
+    console.log(this.card)
+  }
+}
+</script>
+
+<style>
+
+</style>
--- src/views/RoomOverview.vue
+++ src/views/RoomOverview.vue
@@ -1,14 +1,31 @@
 <template>
 <b-container>
-    <headline headline="Wolter"></headline>
+    <headline headline="Test"></headline>
+    <b-row>
+        <b-col v-for="rooms in room" :key="rooms.id">
+            <roomCard :card="rooms"> </roomCard>
+        </b-col>
+    </b-row>
 </b-container>
 </template>
 
 <script>
 import headline from '@/components/base/Headline'
+import roomCard from '@/components/rooms/RoomCard'
+import { roomService } from '@/services/room.service'
 export default {
   components: {
-    headline
+    headline,
+    roomCard
+  },
+  data: function () {
+    return {
+      room: {}
+    }
+  },
+  async mounted () {
+    this.room = await roomService.getRooms()
+    console.log('mounted -> rooms', this.room)
   }
 }
 </script>
