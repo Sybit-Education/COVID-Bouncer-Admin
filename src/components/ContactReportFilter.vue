13d2ccb6201df73fed5f67d4bc03abef2e899852
Alexander Herrmann <alexander.herrmann@sybit.de>
2020-12-10 11:16:36 +0100

    select & datepicker

--- src/components/ContactReportFilter.vue
+++ src/components/ContactReportFilter.vue
@@ -1,11 +1,31 @@
 <template>
   <div id="contact-report-filter">
-    Filter...
+      <b-form inline>
+        <label class="sr-only" for="inline-form-input-name">Name</label>
+        <b-form-select
+          id="inline-form-input-name"
+          class="mb-2 mr-sm-2 mb-sm-0"
+          v-model="selected"
+          :options="user"
+        ></b-form-select>
+          <b-form-datepicker id="example-datepicker" class="mb-2 mr-sm-2 mb-sm-0"></b-form-datepicker>
+        <b-form-datepicker id="example-datepicker"  class="mb-2 mr-sm-2 mb-sm-0"></b-form-datepicker>
+        <b-button variant="primary">Show Contacts</b-button>
+      </b-form>
   </div>
 </template>
 
 <script>
 export default {
+  data () {
+    return {
+      selected: null,
+      user: [
+        { value: null, text: 'Please select an user' },
+        { value: 'ssr', text: 'Stephan S.' }
+      ]
+    }
+  }
 
 }
 </script>
