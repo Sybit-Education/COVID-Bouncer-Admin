c01d1efb5093b885f6a86b20d9e308b628736e85
Degener <jde@sybit.de>
2020-12-10 11:39:21 +0100

    add dashboard card

---
+++ src/components/base/DashboardCard.vue
@@ -0,0 +1,52 @@
+<template>
+  <div>
+  <b-card
+    img-src="https://picsum.photos/900/250/?image=3"
+    img-alt="Card Image"
+    :title="this.title"
+    class="card my-4"
+    @click="router()"
+  >
+    <b-card-text>
+      {{text}}
+    </b-card-text>
+  </b-card>
+</div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      default: 'title'
+    },
+    text: {
+      type: String,
+      default: 'text'
+    },
+    link: {
+      type: String,
+      default: 'link'
+    }
+  },
+  methods: {
+    router () {
+      this.$router.push(this.link)
+    }
+  }
+}
+</script>
+
+<style scoped>
+.card {
+transition: box-shadow .3s ;
+transition: all .5s ease;
+}
+.card:hover {
+  transform: scale(1.03);
+  transition: all .4s ease;
+  box-shadow: 0 0 30px rgba(33,33,33,.2);
+  cursor: pointer;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,3 +1,42 @@
 <template>
-  <h1>Login works!</h1>
+<b-container>
+  <b-row>
+    <b-col v-for="cards in card" :key="cards.id" cols=4>
+      <dashboardCard :title="cards.title" :text="cards.text" :link="cards.link"></dashboardCard>
+    </b-col>
+  </b-row>
+  </b-container>
 </template>
+<script>
+import dashboardCard from '@/components/base/DashboardCard'
+export default {
+  data: function () {
+    return {
+      card: {
+        raumErstellen: {
+          title: 'Raum anlegen',
+          text: 'Hier können sie einen Raum anlegen',
+          link: 'test1'
+        },
+        raumVerwalten: {
+          title: 'Raum verwalten',
+          text: 'Hier können sie einen Raum verwalten',
+          link: 'test2'
+        },
+        raumListe: {
+          title: 'Raum liste',
+          text: 'Hier können sie eine Liste sehen',
+          link: 'test3'
+        }
+      }
+    }
+  },
+  components: {
+    dashboardCard
+  }
+}
+</script>
+
+<style>
+
+</style>
