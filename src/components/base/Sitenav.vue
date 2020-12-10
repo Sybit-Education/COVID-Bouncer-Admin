f679bdbede2c15ad6658d0836267bea8a8fade6c
chrisWolter <wolter-christian@outlook.com>
2020-12-10 11:58:11 +0100

    Create Sitenav

--- src/components/base/Sitenav.vue
+++ src/components/base/Sitenav.vue
@@ -1,3 +1,39 @@
 <template>
-  <h1>Test</h1>
+  <div v-if="showNavbar" class="sitenav">
+    <div class="sidenav-content">
+    <h4 class="sidenav-link">Test</h4>
+    </div>
+  </div>
 </template>
+
+<script>
+export default {
+  name: 'sitenav',
+  computed: {
+    showNavbar () {
+      return this.$route.meta.displayNavbar ?? true
+    }
+  }
+}
+</script>
+
+<style lang="sass" scoped>
+@import '../../assets/variables'
+.sitenav
+  position: fixed
+  width: $sitenav-size
+  height: 100%
+  background: linear-gradient( rgba(0, 0, 0, 0.9), rgba(0, 0, 0, 0.5) ), url("../../assets/img/sitenav-bg.jpg")
+  background-position: center
+  background-size: cover
+  background-repeat: no-repeat
+  box-shadow: 2px 0px 10px gray
+  border-top-right-radius: 8px
+  border-bottom-right-radius: 8px
+
+.sidenav-content
+  padding: 16px
+
+.sidenav-link
+  color: white
+</style>
