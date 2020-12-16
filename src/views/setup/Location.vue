380d1800739c0bfb76b25c0903a2cab486b6f1f8
mirko-pleli <Mirko.Pleli@sybit.de>
2020-12-16 12:35:02 +0100

    Correct rows, error handling via state, correct data structure.

--- src/views/setup/Location.vue
+++ src/views/setup/Location.vue
@@ -1,39 +1,52 @@
 <template>
-  <b-row class="mt-3 align-content-center">
-    <b-col class="location">
-      <b-col>
-        <h3 class="text-center pb-3">Setup from the COVID-Bouncer Admin</h3>
-      </b-col>
+  <b-container class="mt-3 align-content-center">
+    <b-row>
+      <h3 class="text-center pb-3">Welcome to the Setup from the COVID-Bouncer Admin</h3>
+    </b-row>
+    <b-row>
       <form>
-        <b-col>
-          <b-form-group label="2. Set up a Location">
-            <b-form-input
-              id="input-location"
-              class="input-styling"
-              type="text"
-              v-model="location"
-              min="5"
-              max="100">
-            </b-form-input>
-            <b-form-text id="initials-help-block">
-              This step can be skipped.
-            </b-form-text>
-          </b-form-group>
-        </b-col>
-        <b-col>
-          <b-button class="mt-2 sy-background nextButton" @click="addLocation">Next step</b-button>
-          <b-button class="float-right sy-background nextButton" @click="skipStep">Skip step</b-button>
-        </b-col>
+        <b-form-group
+          label="2. Step: Setup a location"
+          id="location"
+          class="input-styling"
+          :invalid-feedback="invalidFeedback">
+          <b-form-input
+            id="input-location"
+            v-model="location"
+            :state="invalidFeedback"
+            trim>
+          </b-form-input>
+          <b-form-text id="initials-help-block">
+            This step can be skipped
+          </b-form-text>
+          <!-- This will only be shown if the preceding input has an invalid state -->
+          <b-form-invalid-feedback id="input-live-feedback">
+            Enter at least 5 letters
+          </b-form-invalid-feedback>
+          <b-row>
+            <b-col>
+              <b-button class="mt-2 sy-background nextButton" v-if="invalidFeedback" @click="addLocation">Next step
+              </b-button>
+            </b-col>
+            <b-col>
+              <b-button class="mt-2 float-right sy-background nextButton" v-if="!invalidFeedback" @click="skipStep">Skip step</b-button>
+            </b-col>
+          </b-row>
+        </b-form-group>
       </form>
-    </b-col>
-  </b-row>
+    </b-row>
+  </b-container>
 </template>
 
 <script>
 import { locationService } from '@/services/location.service'
 
 export default {
-  name: 'Location',
+  computed: {
+    invalidFeedback () {
+      return this.location.length > 5
+    }
+  },
   data () {
     return {
       location: ''
