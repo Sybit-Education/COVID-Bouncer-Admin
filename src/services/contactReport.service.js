db435363be992d5f1c631b3c8d2f23072bf7ca25
Stephan Strittmatter <stephan.strittmatter@sybit.de>
2020-12-16 08:18:35 +0100

    Remove comments

--- src/services/contactReport.service.js
+++ src/services/contactReport.service.js
@@ -1,18 +1,15 @@
 import { $db } from '@/services/firebase.service'
-// import { roomService } from '@/services/room.service'
 class ContactReportService {
   async getContactsOfUserByID (userId, date) {
-    // let myRoom
     const userList = []
     const userRef = await $db().doc('User/' + userId)
     const querySnapshot = await $db().collectionGroup('CheckIn')
       .where('date', '==', date).get()
+
     querySnapshot.forEach((doc) => {
-      // const roomID = doc.ref.parent.parent.id
       const userRefs = doc.data().user
       userRefs.forEach(user => {
         if (user.id === userRef.id) {
-          // myRoom = roomID
           userRefs.forEach(u => {
             u.get().then(user => {
               userList.push(user.data())
@@ -21,7 +18,7 @@ class ContactReportService {
         }
       })
     })
-    // return await roomService.getRoomByID(myRoom)
+
     return userList
   }
 }
