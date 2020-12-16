e56d2758409b64defef44af04cadc8d90ea4a248
Stephan Strittmatter <stephan.strittmatter@sybit.de>
2020-12-16 08:10:29 +0100

    add print button

--- src/components/ContactReportFilter.vue
+++ src/components/ContactReportFilter.vue
@@ -3,7 +3,7 @@
     <b-row>
       <b-form inline >
         <b-form-select
-          class="mb-2 mr-sm-2 mb-sm-0"
+          class="mb-2 mr-sm-2"
           v-model="selectedUser"
           :options="users"
         >
@@ -18,16 +18,31 @@
             :date-format-options="{ year: 'numeric', month: 'numeric', day: 'numeric' }"
             :min="minDate"
             :max="maxDate"
-            class="mb-2 mr-sm-2 mb-sm-0" />
+            class="mb-2 mr-sm-2" />
         <b-button
           variant="primary"
+          type="submit"
           @click="getContactById()"
-          class="d-print-none">Show Contacts</b-button>
+          class="d-print-none mb-2 mr-sm-2">
+          Show Contacts
+        </b-button>
+        <b-button
+          :disabled="!printEnabled"
+          @click="print()"
+          class="d-print-none auto mb-2 ml-sm-5">
+          Print ...
+        </b-button>
       </b-form>
     </b-row>
     <b-row>
-      <b-table :items="contacts" :fields="fields" show-empty id="contact-report-table">
-        <template #empty='scope' >{{scope.emptyText}}</template>
+      <b-table
+        id="contact-report-table"
+        :items="contacts"
+        :fields="fields"
+        show-empty>
+        <template #empty="scope">
+          {{ scope.emptyText }}
+        </template>
       </b-table>
     </b-row>
   </div>
@@ -53,6 +68,11 @@ export default {
       selectedDate: ''
     }
   },
+  computed: {
+    printEnabled () {
+      return this.contacts != null && this.contacts.length > 0
+    }
+  },
   methods: {
     async fetchUser () {
       const response = await userService.getAllUser()
@@ -63,6 +83,9 @@ export default {
     async getContactById () {
       const response = await contactReportService.getContactsOfUserByID(this.selectedUser, this.selectedDate)
       this.contacts = response
+    },
+    print () {
+      window.print()
     }
   },
   mounted () {
